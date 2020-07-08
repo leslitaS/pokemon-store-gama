4a021a0b2049c094e28a809c817d69d7a0fb818d
leslitaS <leslita.scg@gmail.com>
2020-07-08 18:20:13 -0300

    Updates

---
+++ precache-manifest.89cadebf0d70a7fb441eb9b9b0f38c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f912ce11be003958a86cc61e86923c",
+    "url": "/pokemon-store-gama/index.html"
+  },
+  {
+    "revision": "1c4f9a4127c13ea1ceca",
+    "url": "/pokemon-store-gama/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9c9348357b07c67202f4",
+    "url": "/pokemon-store-gama/static/css/main.5b44bee2.chunk.css"
+  },
+  {
+    "revision": "1c4f9a4127c13ea1ceca",
+    "url": "/pokemon-store-gama/static/js/2.3cd8fc5f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pokemon-store-gama/static/js/2.3cd8fc5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9348357b07c67202f4",
+    "url": "/pokemon-store-gama/static/js/main.63872026.chunk.js"
+  },
+  {
+    "revision": "8a1893f6f65ce9fa08a7",
+    "url": "/pokemon-store-gama/static/js/runtime-main.0607a1f4.js"
+  }
+]);
